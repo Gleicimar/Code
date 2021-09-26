92c3fc468be89d3d4354b3a25e8ae2a189d629cd
Gleicimar <gleicimarribeiro@gmail.com>
2021-09-26 15:33:38 -0300

    Add files via upload
    atualizadomobile e css

--- js/sript.js
+++ js/sript.js
@@ -1,19 +1,19 @@
 
 
-const btnMobile =  document.getElementById('btn');
+   const btnMobile = document.getElementById('btn');
 
-function toggleMenu(event ){
-if(event ==='touchstart') event.preventDefault( );
-    const nav  =  document.getElementById( 'nav' );
-    nav.classList.toggle( 'active' );
-    event.currentTarget.setAttribute('aria-expanded', 'active');
-    const active = nav.classList.contains('active');
-    if(active){
-        event.currentTarget.setAttribute('aria-label','Fechar Menu');
-    }else{
-        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
-    }
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
 }
-    btnMobile.addEventListener('click', toggleMenu);
-    btnMobile.addEventListener('touchstart', toggleMenu,{passive: true});
-   /*document.addEventListener('touchstart', toggleMenu, {passive: true});
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
